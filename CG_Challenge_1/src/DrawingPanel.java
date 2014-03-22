bfb4f7ffd0c58f74650c9c02b471e202216d06e2
Santiago Zubieta <santiago.zubieta@gmail.com>
2014-03-22 16:52:47 -0500

    Cleaned Challenge 1 code a bit

--- CG_Challenge_1/src/DrawingPanel.java
+++ CG_Challenge_1/src/DrawingPanel.java
@@ -10,28 +10,24 @@ import java.awt.Color;
 import javax.swing.JPanel;
 import javax.swing.JOptionPane;
 
-public class DrawingPanel extends JPanel implements MouseListener,
-        MouseMotionListener {
-    private BufferedImage curImage; // Current BG image to avoid re-drawing all
-                                    // shapes
-    private Graphics2D curImageG2D; // Graphics component of the current image
+public class DrawingPanel extends JPanel implements MouseListener, MouseMotionListener {
+    private BufferedImage curImage;   // Current BG image to avoid re-drawing all shapes
+    private Graphics2D curImageG2D;   // Graphics component of the current image
     private ArrayList<Circle> circAr; // List of Circles in screen;
-    private ArrayList<Line> lineAr; // List of Lines in screen;
-    private ArrayList<Integer> dotX; // List of random X coordinates for 500
-                                        // points
-    private ArrayList<Integer> dotY; // List of random Y coordinates for 500
-                                        // points
-    private Window window; // The parent Window object
-    private String status; // Current application status
-    private int radius; // Selected radius of drawn Circles
-    private int lines; // Selected amount of drawn Lines
-    private int midX; // X coordinate of panel center
-    private int midY; // Y coordinate of panel center
-    private int dimX; // Width of frame/panel
-    private int dimY; // Height of frame/panel
-    private int curX; // Current mouse X position
-    private int curY; // Current mouse Y position
-    private int INF; // A very large number
+    private ArrayList<Line> lineAr;   // List of Lines in screen;
+    private ArrayList<Integer> dotX;  // List of random X coordinates for 500 points
+    private ArrayList<Integer> dotY;  // List of random Y coordinates for 500 points
+    private Window window;            // The parent Window object
+    private String status;            // Current application status
+    private int radius;               // Selected radius of drawn Circles
+    private int lines;                // Selected amount of drawn Lines
+    private int midX;                 // X coordinate of panel center
+    private int midY;                 // Y coordinate of panel center
+    private int dimX;                 // Width of frame/panel
+    private int dimY;                 // Height of frame/panel
+    private int curX;                 // Current mouse X position
+    private int curY;                 // Current mouse Y position
+    private int INF;                  // A very large number 
 
     // Constructor method
     public DrawingPanel(Window window) {
@@ -56,8 +52,7 @@ public class DrawingPanel extends JPanel implements MouseListener,
         curImageG2D = null;
     }
 
-    // When the status of the application is changed by a button from
-    // OptionPanel
+    // When the status of the application is changed by a button from OptionPanel
     public void changeStatus(String newStatus) {
         resetCurrentImage();
         curX = curY = INF;
@@ -108,29 +103,26 @@ public class DrawingPanel extends JPanel implements MouseListener,
             // This will ask for the radius of the Circle that is going to be
             // drawn
             // after every time the user presses the mouse over the drawing area
-            String res = JOptionPane
-                    .showInputDialog("Enter radius of desired Circle");
+            String res = JOptionPane.showInputDialog("Enter radius of desired Circle");
             try {
                 radius = Integer.parseInt(res);
-            } catch (Exception e) {
+            }
+            catch (Exception e) {
                 radius = 1; // by default if it can't parse an integer, the
-                            // radius
-                // will be one.
+                            // radius will be one.
             }
             generateCircle();
         }
         if (status == "LINER") {
             // This will ask for the amount of Lines that are going to be drawn
-            // after
-            // every time the user presses the mouse over the drawing area
-            String res = JOptionPane
-                    .showInputDialog("Enter desired amount of Lines:");
+            // after every time the user presses the mouse over the drawing area
+            String res = JOptionPane.showInputDialog("Enter desired amount of Lines:");
             try {
                 lines = Integer.parseInt(res);
-            } catch (Exception e) {
+            } 
+            catch (Exception e) {
                 lines = 0; // by default if it can't parse an integer, the
-                            // amount of
-                // lines will be 0.
+                           // amount of lines will be 0.
             }
             generateRandomLines();
         }
@@ -160,12 +152,9 @@ public class DrawingPanel extends JPanel implements MouseListener,
 
     private void doCursor(Graphics2D g2d) {
         // A hovering sign with the current coordinates of the mouse, this one
-        // is
-        // not drawn into the currentImage because the idea is to show the
-        // coordi-
-        // nates of the cursor hovering the image, which instead of being
-        // re-ren-
-        // dered piece by piece, it is just loaded and then this is drawn above.
+        // ..is not drawn into the currentImage because the idea is to show the
+        // ..coordinates of the cursor hovering the image, which instead of being
+        // ..re-rendered piece by piece, it is just loaded and then this is drawn.
         g2d.setColor(new Color(64, 64, 64));
         String pos = curX + ", " + curY * -1;
         int mx = curX + midX;
@@ -217,8 +206,7 @@ public class DrawingPanel extends JPanel implements MouseListener,
     }
 
     private void paintDots(Graphics2D g2d) {
-        // Draw in the screen the 500 random dots to draw Lines between
-        // afterwards
+        // Draw in the screen the 500 random dots to draw Lines between afterwards
         for (int i = 0; i < dotX.size(); ++i) {
             int x = dotX.get(i);
             int y = dotY.get(i);
@@ -264,10 +252,10 @@ public class DrawingPanel extends JPanel implements MouseListener,
         if (x1 == x2) {
             int INF = 2142; // a really 'big' slope.
             m = INF; // if its a vertical line (x1 == x2), then we asume a slope
-            // with an arbitrary value which absolute value is greater than 1,
-            // so when swapping coordinates, it will be seen as a horizontal
-            // line
-        } else {
+            // ..with an arbitrary value which absolute value is greater than 1,
+            // ..so when swapping coordinates, it will be seen as a horizontal line
+        } 
+        else {
             m = (double) (dy) / (double) (dx); // the real slope of the
             // line, will be preserved ever after the line is 'truncated' so we
             // can tell it was 'truncated', and if it was, then the pixels are
@@ -277,27 +265,19 @@ public class DrawingPanel extends JPanel implements MouseListener,
         int dir = (m < 0) ? -1 : 1; // Direction of vertical change
 
         // Algorithm requires dx and dy to be positive, having an absolute dx
-        // doesnt
-        // matter from here on because it will start from the leftmost x until
-        // the
-        // rightmost x (a positive direction), and having an absolute dy doesnt
-        // matter
-        // because it will be only used for calculation, direction of change is
-        // set
-        // individualy to be either positive or negative.
+        // ..doesnt matter from here on because it will start from the leftmost x 
+        // ..until the rightmost x (a positive direction), and having an absolute 
+        // ..dy doesnt matter because it will be only used for calculation, direc-
+        // ..tion of change is set individualy to be either positive or negative.
         dy = Math.abs(dy);
         dx = Math.abs(dx);
 
         if (Math.abs(m) > 1) {
             // If the absolute value of the slope is greater than one, this will
-            // get
-            // a new line where the absolute value is less than one, for the
-            // algorithm
-            // to work. The original slope is preserved so when pixels are being
-            // drawn,
-            // if the original absolute value was greater than one, the
-            // coordinates are
-            // swapped back at the time of printing.
+            // ..get a new line where the absolute value is less than one, for the
+            // ..algorithm to work. The original slope is preserved so when pixels 
+        	// ..are being drawn, if the original absolute value was greater than 
+        	// ..one, the coordinates are swapped back at the time of printing.
             int temp = dx;
             dx = dy;
             dy = temp;
@@ -311,10 +291,8 @@ public class DrawingPanel extends JPanel implements MouseListener,
 
         if (x2 < x1) {
             // This is so we can use the absolute value of dx, since we are
-            // starting
-            // always at the leftmost point, going in a positive direction to
-            // the one
-            // that its at the rightmost.
+            // ..starting always at the leftmost point, going in a positive 
+        	// ..direction to the one that its at the rightmost.
             int temp = x2;
             x2 = x1;
             x1 = temp;
@@ -329,12 +307,9 @@ public class DrawingPanel extends JPanel implements MouseListener,
         int incE = 2 * dy; // increment to east
         int incNE = 2 * dy - 2 * dx; // increment to northeast
         // Whats REALLY fast about this algorithm (and precise) is that it only
-        // uses
-        // addition and comparissons, the only needed multiplications (which
-        // increases
-        // error) are computed only once. also, it only uses integers, and that
-        // gives
-        // a lot of precission and speed.
+        // ..uses addition and comparissons, the only needed multiplications (which
+        // ..increases error) are computed only once. also, it only uses integers, 
+        // ..and that gives a lot of precission and speed.
         while (dx > 0) {
             if (d <= 0) {
                 d += incE;
@@ -377,8 +352,7 @@ public class DrawingPanel extends JPanel implements MouseListener,
             // symmetry, it can be easily fixed for all the other octants, this
             // is by trying all possible combinations of place and signs of the
             // x and y coordinates, and adding the current position of the
-            // mouse,
-            // without changing that one!
+            // mouse, without changing that one!
             oct1.add(new Point( x,  y));
             oct2.add(new Point( y,  x));
             oct3.add(new Point( y, -x));
@@ -447,10 +421,8 @@ public class DrawingPanel extends JPanel implements MouseListener,
         super.paintComponent(g2d); // Clears the screen
         if (curImage == null) {
             // This is to avoid re-rendering each of the circles/lines when the
-            // screen
-            // is refreshed (which happens due to the constantly showing of the
-            // current
-            // mouse coordinates.
+            // ..screen is refreshed (which happens due to the constantly showing 
+        	// ..of the current mouse coordinates.
             curImage = new BufferedImage(dimX, dimY,
                     BufferedImage.TYPE_INT_ARGB);
             curImageG2D = curImage.createGraphics();
@@ -464,11 +436,9 @@ public class DrawingPanel extends JPanel implements MouseListener,
                 doCircles(g2d);
             doAxis(g2d);
         } else {
-            // If the image is not null, it means nothing has made it null
-            // (like,
-            // changing between modes, resetting a mode, inserting a new circle
-            // or
-            // changing the desired amount of lines) so simply draw the image
+            // If the image is not null, it means nothing has made it null (like, 
+        	// ..changing between modes, resetting a mode, inserting a new circle 
+        	// ..or changing the desired amount of lines) so simply draw the image
             g2d.drawImage(curImage, null, 0, 0);
         }
         doCursor(g2d);
